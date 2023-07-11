b1c28787aa5a8c907a03b93ecb15ff77c351682c
Divyansh srivastava <85442869+divyaannsh@users.noreply.github.com>
2023-07-11 19:16:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+//let score;
+
+//score= 10;
+
+//console.log(score);
+
+//sting,Numbers, boolean, null,undefined,symbol
+
+/*const name = 'divyansh';
+const age = 39;
+const iscool = true;
+const rating = 4.5;
+const x = null;
+const y= undefined;
+let z;*/
+
+
+/*console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);*/
+
+//concatenation
+
+//console.log('My name is ' + name + 'snd i am '+ age);
+
+//template string
+// const hello = 'My name is ${name} and i am ${age}';
+
+ //.log(hello);
+
+//how to find length of of string
+/*const s ='hello world';
+console.log(s.length);
+//upper&lowercase
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+console.log(s.substring(0, 5));
+
+console.log(s.split(""))
+
+//array-variable that hold multiple values
+
+const druts =[ 'apple','orange','pears']
+
+console.log(druts);
+
+console.log(druts[1]);*///positioning of element
+
+
+  //for loops
+
+ /* for(let i = 0; i <= 10; i++) {
+    console.log('For Loop Number: ${i} ');
+  }*/
+
+  //while
+
+  //let i=0;
+ // while(i<10) {
+  //  console.log('While loop Number: ${i}');
+  //  i++;
+  //}
+
+//ifelse
+
+/*const x=10;
+if(x== 20){
+    console.log('x is 20');
+}else if(x>10) {
+    console.log('x is nnot 20');
+}else {
+    console.log('x is less than 20');
+}
+  */
+//ternary opt
+/*const x = 10;
+
+const color = x > 10 ? 'red' : 'blue';
+
+console.log(color);
+*/
+//switch case 
+/*const x=1;
+
+const color = x > 10 ? 'red'  : 'blue';
+
+switch (color){
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+        default:
+            console.log('color is NOtred or blue');
+            break;
+}*/
+
+
+//function
+/*function addNums(num1, num2 ){
+    return num1 + num2;
+}
+console.log(addNums(5,5));*/
+
+//constructer function
+
+/*class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+
+    }
+}
+
+//instantiate object
+   
+const person1 = new Person('John','DOE','4-4-1980');
+const person2 = new Person('Johny','smith','4-6-1970');
+
+console.log(person1.firstName);*/
+
+   // document object model
+
+
+
+
+   console.log(window);
+   
+//single element
+document
+
+
+
+
+
+
+
+
+
+
+
+
+
+
